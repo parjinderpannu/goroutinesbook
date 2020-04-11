8eb2027fbd7fb9df2d758cc3674066c69dd90714
PARJINDER S PANNU <parjinderpannu@gmail.com>
2020-04-11 15:26:11 -0700

    restricting go func chan direction as receive only or send only

--- chandemo/main.go
+++ chandemo/main.go
@@ -11,12 +11,12 @@ func main() {
 	ch := make(chan int, 1)
 
 	wg.Add(2)
-	go func(ch chan int, wg *sync.WaitGroup) {
+	go func(ch <-chan int, wg *sync.WaitGroup) {
 		fmt.Println(<-ch)
 		ch <- 27
 		wg.Done()
 	}(ch, wg)
-	go func(ch chan int, wg *sync.WaitGroup) {
+	go func(ch chan<- int, wg *sync.WaitGroup) {
 		ch <- 42
 		time.Sleep(5 * time.Millisecond)
 		fmt.Println(<-ch)
