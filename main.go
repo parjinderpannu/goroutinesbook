bd40a7b316ead45855c5268482e2591398f785d9
PARJINDER S PANNU <parjinderpannu@gmail.com>
2020-04-11 09:30:24 -0700

    using waitgroup vs sleep

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
@@ -10,24 +11,28 @@ var cache = map[int]Book{}
 var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 func main() {
+	wg := &sync.WaitGroup{}
 	for i := 0; i < 10; i++ {
 		id := rnd.Intn(10) + 1
-		go func(id int) {
+		wg.Add(2)
+		go func(id int, wg *sync.WaitGroup) {
 			if b, ok := queryCache(id); ok {
 				fmt.Println("from cache")
 				fmt.Println(b)
 			}
-		}(id)
-		go func(id int) {
+			wg.Done()
+		}(id, wg)
+		go func(id int, wg *sync.WaitGroup) {
 			if b, ok := queryDatabase(id); ok {
 				fmt.Println("from database")
 				fmt.Println(b)
 			}
-		}(id)
+			wg.Done()
+		}(id, wg)
 		time.Sleep(150 * time.Millisecond)
 	}
 
-	time.Sleep(2 * time.Second)
+	wg.Wait()
 }
 
 func queryCache(id int) (Book, bool) {
